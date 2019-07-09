a440741cde967e567b3554e231862bea5d80ec74
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-07-09 23:06:46 +0600

    updated model

--- db/migrate/20190709162645_create_friend_requests.rb
+++ db/migrate/20190709162645_create_friend_requests.rb
@@ -3,12 +3,15 @@ class CreateFriendRequests < ActiveRecord::Migration[5.2]
     create_table :friend_requests do |t|
       t.integer :sender_id
       t.integer :receiver_id
+      t.integer :accepted
 
       t.timestamps
     end
 
     add_index :friend_requests, :sender_id
     add_index :friend_requests, :receiver_id
+    add_index :friend_requests, :accepted
+    add_index :friend_requests, [:sender_id, :receiver_id] , unique: true
     add_foreign_key :friend_requests, :users, column: :sender_id
     add_foreign_key :friend_requests, :users, column: :receiver_id
   end

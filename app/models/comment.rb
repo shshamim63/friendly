2d96f0dc8a0c0bb7b34046101a186cc4d4955a0a
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-07-10 16:54:34 +0600

    added changes to model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  has_many :replies, class_name: "comment",
+                            foreign_key: "reply_id"
+  belongs_to :, class_name: "comment"
 end

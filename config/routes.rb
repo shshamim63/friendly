b6ddd49930804bf7c6a28483fbc690b5a2437be0
trillianjose <trillianjosea@gmail.com>
2019-07-18 14:13:27 +0200

    user controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

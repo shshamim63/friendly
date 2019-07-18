03ef0d96f48cbdedc411e7072f86ca58afb45e34
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-07-18 18:58:40 +0600

    started working on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index]
+
   def index
+    @friends = current_user.friends
+    @posts = current_user.posts + current_user.friends.posts
   end
 
   def new

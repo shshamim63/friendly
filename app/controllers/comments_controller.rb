b53ba3e14a574f04f36b33f15da0beead06d3688
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-07-23 17:22:07 +0600

    comment creating error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
-
+ before_action :find_post
   def create
     @comment = @post.comments.build(user_id: current_user.id, content: params[:content])
 
     if @comment.save
       redirect_to posts_path
-      flash.now[:success] = 'Post was successfully created.'
+      flash.now[:success] = 'Comment was successfully created.'
     else
       render 'new'
     end

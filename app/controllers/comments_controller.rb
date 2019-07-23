5e62cea05b72273ed7498ff4d05174a29a7221a8
trillianjose <trillianjosea@gmail.com>
2019-07-23 14:02:03 +0200

    comment form fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,18 @@
 class CommentsController < ApplicationController
- before_action :find_post
+  before_action :find_post
+
   def create
-    @comment = @post.comments.build(user_id: current_user.id, content: params[:content])
+    @comment = @post.comments.build(user_id: current_user.id, content: params[:comment][:content])
 
     if @comment.save
       redirect_to posts_path
       flash.now[:success] = 'Comment was successfully created.'
-    else
-      render 'new'
     end
   end
-
 
   private
 
   def find_post
     @post = Post.find(params[:post_id])
   end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,6 +50,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:content, :user_id)
+    params.require(:post).permit(:content)
   end
 end

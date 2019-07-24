905d3708cdba72efc2cbdec589e4d0edc62a18b8
trillianjose <trillianjosea@gmail.com>
2019-07-24 13:58:18 +0200

    fixing errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :posts  do
     resources :comments
   end
-  resources :users
+
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

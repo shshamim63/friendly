87073cc4a6358a8ef06d6e14b532743d1e42b5f7
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-07-27 17:18:36 +0600

    added change to like

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -9,4 +9,15 @@ module LikesHelper
         update_likes_path(object_type: type, object_id: id, like: { reaction_type: reaction })
       end
   end
+
+  def liker_method(type, id, reaction )
+    like = Like.find_by(object_type: type , object_id: id, user_id: current_user)
+      if like.nil?
+        :post
+      elsif like.reaction_type == reaction
+        :delete
+      else
+        :put
+      end
+  end
 end

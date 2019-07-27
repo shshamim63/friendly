486a0e25e2dcf8bc7c473d5752222f57c335c88b
trillianjose <trillianjosea@gmail.com>
2019-07-27 15:12:43 +0200

    fixed error on index, refactorized likes

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,23 +1,13 @@
 module LikesHelper
-  def liker(type, id, reaction )
+  def liker(type, id, reaction)
     like = Like.find_by(object_type: type , object_id: id, user_id: current_user)
-      if like.nil?
-        create_likes_path(object_type: type, object_id: id, like: { reaction_type: reaction })
-      elsif like.reaction_type == reaction
-        destroy_likes_path(object_type: type, object_id: id, user_id: current_user, like: { reaction_type: reaction })
-      else
-        update_likes_path(object_type: type, object_id: id, like: { reaction_type: reaction })
-      end
-  end
 
-  def liker_method(type, id, reaction )
-    like = Like.find_by(object_type: type , object_id: id, user_id: current_user)
-      if like.nil?
-        :post
-      elsif like.reaction_type == reaction
-        :delete
-      else
-        :put
-      end
+    if like.nil?
+      [create_likes_path(object_type: type, object_id: id, like: { reaction_type: reaction }), :post]
+    elsif like.reaction_type == reaction
+      [destroy_likes_path(object_type: type, object_id: id, user_id: current_user, like: { reaction_type: reaction }), :delete]
+    else
+      [update_likes_path(object_type: type, object_id: id, like: { reaction_type: reaction }), :put]
+    end
   end
 end

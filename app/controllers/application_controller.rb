e5a87234ab1368e1b23a17d6c008db628b472664
trillianjose <trillianjosea@gmail.com>
2019-07-30 15:19:55 +0200

    changes made to images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :last_name, :first_name, :birthday, :image, :gender ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :last_name, :first_name, :birthday, :avatar, :gender ])
   end
 end

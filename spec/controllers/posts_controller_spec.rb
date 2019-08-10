07c1dcc46e1c5e3ae77bd3ab5a5977f5298c0a19
trillianjose <trillianjosea@gmail.com>
2019-08-10 20:47:39 +0200

    nothing to add

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  let!(:first_user) { create(:user) }
+  let!(:first_post) { create(:post) }
+  let!(:second_post) { create(:post) }
+
+  before { sign_in first_user }
+
+
 end
